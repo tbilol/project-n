69fd56e046b2e6d94ab4226548183b53f28378fe
bilol <tbilol2908@gmail.com>
2025-05-15 17:53:17 +0500

    classlar ochildi

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        /*
+        /*           for: Bilol
         Admin
         1. Add book
         2. Edit book
@@ -8,7 +8,7 @@ public class Main {
         4. Users
         0. Exit
 
-        User
+        User       for: Omonjon
         1. Show books
         2. Buy book
         3. My books
