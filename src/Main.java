a541145d10d48fe1e1f7e302380e9690be29a708
bilol <tbilol2908@gmail.com>
2025-05-15 17:34:24 +0500

    1 commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Admin
+        1. Add book
+        2. Edit book
+        3. Show books
+        4. Users
+        0. Exit
+
+        User
+        1. Show books
+        2. Buy book
+        3. My books
+        4. Balance
+         */
+    }
+}
