fb4f284093121356fa60dff58b443a0dff63e1ef
bilol <tbilol2908@gmail.com>
2025-05-17 15:20:39 +0500

    2- boshqich

---
+++ src/DbSourse.java
@@ -0,0 +1,11 @@
+import entity.User;
+import entity.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DbSourse {
+    public static List<User> users = new ArrayList<>();
+    public static List<Book> books = new ArrayList<>();
+
+}
--- src/Service/adminService.java
+++ src/Service/adminService.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class adminService {
     static Scanner sc = new Scanner(System.in);
     static Scanner str = new Scanner(System.in);
+    static
     public void menu(){
         System.out.print("""
                 1. Add book
@@ -17,14 +18,27 @@ public class adminService {
             case 0 ->{
                 return;
             }
-            case 1 ->{}
-            case 2 ->{}
-            case 3 ->{}
-            case 4 ->{}
+            case 1 ->{addBook();}
+            case 2 ->{editBook();}
+            case 3 ->{showBooks();}
+            case 4 ->{Users();}
             default ->{
                 System.out.println(" Invalid command! ");
                 menu();
             }
         }
+    }
+
+    private static void addBook() {
+
+    }
+
+    private static void editBook() {
+    }
+
+    private static void showBooks() {
+    }
+
+    private static void Users() {
     }
 }
